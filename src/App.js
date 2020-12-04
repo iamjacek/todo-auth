76c993b0c408a92fc2e5b18a6018b4d13de16cdc
iamjacek <iamjacek@icloud.com>
2020-12-04 00:11:59 +0000

    nav, routes and Home, Lists pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Lists from "./pages/Lists"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/lists">Lists</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/lists" component={Lists} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Lists.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Lists = () => {
+  return <div>Your Lists</div>
+}
+
+export default Lists
