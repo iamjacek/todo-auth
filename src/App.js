b56cfd755f267ade5ac710647b41e3b4dc6a7739
iamjacek <iamjacek@icloud.com>
2020-12-06 11:34:28 +0000

    private route, Context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import Lists from "./pages/Lists"
 
-function App() {
+import PrivateRoute from './PrivateRoute';
+import { AuthContext } from "./context/auth";
+
+function App(props) {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/lists">Lists</Link>
-          </li>
-        </ul>
-        <Route exact path="/" component={Home} />
-        <Route path="/lists" component={Lists} />
-      </div>
-    </Router>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/lists">Lists</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <PrivateRoute path="/lists" component={Lists} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 
---
+++ src/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+import { useAuth } from "./context/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const isAuthenticated = useAuth()
+
+    return (
+        <Route {...rest} render={(props) => 
+            isAuthenticated ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to="/" />
+        )}>
+        </Route>
+    )
+}
+
+export default PrivateRoute
