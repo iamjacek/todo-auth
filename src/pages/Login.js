2dc664bab15a048006f35c1fa6bf55d2279cc32b
iamjacek <iamjacek@icloud.com>
2020-12-06 18:56:40 +0000

    styled materialUI instead of styled-components, form style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,51 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../img/logo.png'
 
-import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
+import { Box, TextField, Button, Container } from '@material-ui/core'
+
+//add styling using JSS object
+import { styled } from '@material-ui/core/styles'
+
+const Logo = styled(Box)({
+    width: '100px',
+    height: '100px',
+    background: `url(${logo})`,
+    backgroundSize: 'cover',
+    margin: '2rem auto 2rem auto',
+})
+
+const CenterContainer = styled(Container)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+})
+
+const TextFieldMargin = styled(TextField)({
+    marginBottom: '1rem',
+})
+
+const LinkCenter = styled(Link)({
+    textAlign: 'center',
+})
+
+const ButtonBig = styled(Button)({
+    height: '56px',
+    fontSize: '1.1rem'
+})
 const Login = () => {
     return (
         <Box>
-            <Box>
-                <Box><img src={logo} /></Box>
-                <form>
-                    <TextField label="Email" variant="filled" type="email" />
-                    <TextField label="Password" variant="filled" type="password" />
-                    <Button variant="outlined" color="primary">Sign In</Button>
-                </form>
-                <Link to="/signup">Don't have an account?</Link>
-            </Box>
-        </Box>
+        <CenterContainer maxWidth="xs">
+            <Logo />
+            <form>
+                <TextField label="Email" variant="filled" type="email" />
+                <TextFieldMargin label="Password" variant="filled" type="password" />
+                <ButtonBig variant="outlined" color="primary">Sign In</ButtonBig>
+            </form>
+            <LinkCenter to="/login">Don't have an account?</LinkCenter>
+        </CenterContainer>
+    </Box>
     )
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,25 +2,51 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../img/logo.png'
 
-import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { Box, TextField, Button, Container } from '@material-ui/core'
 
+//add styling using JSS object
+import { styled } from '@material-ui/core/styles'
 
+const Logo = styled(Box)({
+    width: '100px',
+    height: '100px',
+    background: `url(${logo})`,
+    backgroundSize: 'cover',
+    margin: '2rem auto 2rem auto',
+})
+
+const CenterContainer = styled(Container)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+})
+
+const TextFieldMargin = styled(TextField)({
+    marginBottom: '1rem',
+})
+
+const LinkCenter = styled(Link)({
+    textAlign: 'center',
+})
+
+const ButtonBig = styled(Button)({
+    height: '56px',
+    fontSize: '1.1rem'
+})
 
 const Signup = () => {
     return (
         <Box>
-            <Box>
-                <Box><img src={logo} alt="to do list" /></Box>
+            <CenterContainer maxWidth="xs">
+                <Logo />
                 <form>
                     <TextField label="Email" variant="filled" type="email" />
                     <TextField label="Password" variant="filled" type="password" />
-                    <TextField label="Password" variant="filled" type="password" />
-                    <Button variant="outlined" color="primary">Sign In</Button>
+                    <TextFieldMargin label="Password" variant="filled" type="password" />
+                    <ButtonBig variant="outlined" color="primary">Sign In</ButtonBig>
                 </form>
-                <Link to="/login">Already have an account?</Link>
-            </Box>
+                <LinkCenter to="/login">Already have an account?</LinkCenter>
+            </CenterContainer>
         </Box>
     )
 }
