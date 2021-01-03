d0f00f5288485cd629618345b2c6b46b8652eac0
iamjacek <jake47s@gmail.com>
2021-01-03 10:36:44 +0000

    add list and add items locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import { BrowserRouter as Router, Link, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
 import Lists from "./pages/Lists"
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
-import PrivateRoute from './PrivateRoute';
-import { AuthContext } from "./context/auth";
+import PrivateRoute from "./PrivateRoute"
+import { AuthContext } from "./context/auth"
 
-import { Avatar, Container, Button } from '@material-ui/core'
-import { styled } from '@material-ui/core/styles'
+import { Avatar, Container, Button } from "@material-ui/core"
+import { styled } from "@material-ui/core/styles"
 
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from "history"
 
-
-import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from "@apollo/client"
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 const apiUrl = process.env.API_URL || "http://localhost:1337/graphql"
-let history = createBrowserHistory();
+let history = createBrowserHistory()
 const client = new ApolloClient({
   uri: apiUrl,
   cache: new InMemoryCache(),
@@ -55,7 +54,7 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens}}>
+      <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
         <Router>
           <Wrapper>
             {/* MENU NAVBAR */}
@@ -68,16 +67,44 @@ function App() {
                   <Link to="/lists">Lists</Link>
                 </li>
               </ul>
-              
-              
+
               <ul>
-              { authTokens && <li>Welcome back {authTokens.user.username}!</li>}
-                { authTokens && <li><Button size="small" variant="outlined" color="primary" onClick={handleSignout} >Sign Out</Button> </li>}
-                { authTokens && <li> <Avatar style={{background: "#fc6e51"}}>{authTokens.user.username.charAt(0).toUpperCase()}</Avatar></li>}
-                { !authTokens && <li> <Link to="/login">Log In</Link></li>}
-                { !authTokens && <li> <Link to="/signup">Register</Link></li>}
+                {authTokens && (
+                  <li>Welcome back {authTokens.user.username}!</li>
+                )}
+                {authTokens && (
+                  <li>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      color="primary"
+                      onClick={handleSignout}
+                    >
+                      Sign Out
+                    </Button>{" "}
+                  </li>
+                )}
+                {authTokens && (
+                  <li>
+                    {" "}
+                    <Avatar style={{ background: "#fc6e51" }}>
+                      {authTokens.user.username.charAt(0).toUpperCase()}
+                    </Avatar>
+                  </li>
+                )}
+                {!authTokens && (
+                  <li>
+                    {" "}
+                    <Link to="/login">Log In</Link>
+                  </li>
+                )}
+                {!authTokens && (
+                  <li>
+                    {" "}
+                    <Link to="/signup">Register</Link>
+                  </li>
+                )}
               </ul>
-              
             </ul>
             <main>
               <Route exact path="/" component={Home} />
@@ -87,10 +114,13 @@ function App() {
             </main>
             <footer>
               <p>
-                <span>ToDo Auth</span> ©{" "}
-                {new Date().getFullYear()}, Built by
+                <span>ToDo Auth</span> © {new Date().getFullYear()}, Built by
                 {` `}
-                <a href="https://www.jacekwitucki.com" target="_blank" rel="noreferrer">
+                <a
+                  href="https://www.jacekwitucki.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Jacek Witucki
                 </a>
                 {` `}
--- client/src/pages/Lists.js
+++ client/src/pages/Lists.js
@@ -1,183 +1,246 @@
-import React from "react"
-import { Box, Container, Typography, Card, Chip, TextField } from '@material-ui/core'
+import React, { useState } from "react"
+import {
+  Box,
+  Container,
+  Typography,
+  Card,
+  Chip,
+  TextField,
+} from "@material-ui/core"
 import Logo from "../components/Logo"
-import Icon from '@material-ui/core/Icon';
+import Icon from "@material-ui/core/Icon"
 
-import { useAuth } from "../context/auth";
+import { useAuth } from "../context/auth"
 
 //add styling using JSS object
-import { styled } from '@material-ui/core/styles'
+import { styled } from "@material-ui/core/styles"
 
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from "@apollo/client"
 
 const LIST_DATA = gql`
-  query{
-    lists{
-      users_permissions_user{
+  query {
+    lists {
+      users_permissions_user {
         _id
       }
       _id
       name
       description
-      items{
+      items {
         itemName
         itemDescription
         _id
       }
     }
   }
-`;
-
-
+`
 
 const CenterContainer = styled(Container)({
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
 })
 
 const Wrapper = styled(Box)({
-display: 'flex',
-
+  display: "flex",
 })
 
 const CardWrapper = styled(Box)({
-  display: 'flex',
-  flexDirection: 'row',
+  display: "flex",
+  flexDirection: "row",
   flexWrap: "wrap",
-  justifyContent: "center"
+  justifyContent: "center",
 })
 
 const CardList = styled(Card)({
-  width: '200px',
-  margin: '0.5rem',
-  padding: '0.5rem 1rem',
+  width: "200px",
+  margin: "0.5rem",
+  padding: "0.5rem 1rem",
+})
+
+const AddItem = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
 })
 
 const handleDelete = () => {
-  console.info('You clicked the delete icon.');
-};
+  console.info("You clicked the delete icon.")
+}
 
-
+var flag = 0
+var flagItems = 0
 
 const Lists = () => {
+  const [customLists, setCustomLists] = useState([])
+
   const { authTokens } = useAuth()
   const ID = authTokens.user._id
-  const { loading, error, data } = useQuery(LIST_DATA);
+  const { loading, error, data } = useQuery(LIST_DATA)
+
+  const addNewList = () => {
+    const newListName = document.querySelector("#newListName").value
+    const newListDesc = document.querySelector("#newListDesc").value
+    if (newListName === null || newListName === "") {
+      console.log("error, name is required")
+    } else {
+      const description = newListDesc || ""
+      setCustomLists([
+        {
+          name: newListName,
+          _id: flag,
+          description: description,
+          items: [],
+        },
+        ...customLists,
+      ])
+      flag++
+      document.querySelector("#newListName").value = ""
+      document.querySelector("#newListDesc").value = ""
+    }
+  }
+
+  const addNewItem = (e, id) => {
+    //check item input ? empty and is unique for a list
+    const newItem = e.currentTarget.previousSibling.getElementsByTagName(
+      "INPUT"
+    )[0].value
+    let arrayIndex
+    if (newItem === null || newItem === "") {
+      console.log("please insert item name before you try to add it")
+    } else {
+      customLists.map((list) => {
+        if (list._id === id) {
+          arrayIndex = customLists.indexOf(list)
+          return arrayIndex
+        } else {
+          return null
+        }
+      })
+
+      let updatedList = [...customLists]
+      console.log(updatedList)
+      let listToBeUpdated = { ...customLists[arrayIndex] }
+      listToBeUpdated.items.push({ itemName: newItem, _id: flagItems })
+      customLists[arrayIndex] = listToBeUpdated
+      flagItems++
+      setCustomLists(updatedList)
+      console.log(customLists)
+    }
+  }
 
   const handleClick = (e) => {
     e.currentTarget.classList.toggle("item__clicked")
-  };
+  }
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <p>Loading...</p>
 
-  if (error) return <p>Error :(</p>;
-    
-  return  (
+  if (error) return <p>Error :(</p>
+
+  return (
     <Wrapper>
       <CenterContainer>
-          <div>
-            <Logo />
-          </div>
-          
-          <Typography variant="h3" component="h1" align="center" >
-            Your Lists!
-          </Typography>
+        <div>
+          <Logo />
+        </div>
 
-          <Typography variant="subtitle1" align="center" paragraph={true} >
-            Simply choose one of your lists or create new
-          </Typography>
-          <CardWrapper>
-            <CardList elevation={3} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-              <h2 style={{marginBottom: "0.2rem"}}>Create new list</h2>
-              <Icon style={{ fontSize: 50 }}>add_circle</Icon>
-              <TextField id="filled-basic" label="Filled" variant="filled" />
+        <Typography variant="h3" component="h2" align="center">
+          Your Lists!
+        </Typography>
+
+        <Typography variant="subtitle1" align="center" paragraph={true}>
+          Simply choose one of your lists or create new
+        </Typography>
+        <CardWrapper id="cardWrapper">
+          <CardList
+            elevation={3}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "left",
+              padding: "1rem",
+            }}
+          >
+            <Typography component="h2" style={{ marginBottom: "0.5rem" }}>
+              Create new list
+            </Typography>
+
+            <TextField id="newListName" label="Name" variant="filled" />
+            <TextField id="newListDesc" label="Description" variant="filled" />
+            <Icon
+              style={{ fontSize: 50, margin: "0.5rem auto 0 auto" }}
+              onClick={() => {
+                addNewList()
+              }}
+            >
+              add_circle
+            </Icon>
+          </CardList>
+
+          {/* **************** CUSTOM LISTS ADDED LOCALLY ******************** */}
+          {customLists.map(({ _id, name, description, items }) => (
+            <CardList elevation={3} key={_id}>
+              <h2 style={{ marginBottom: "0.2rem" }}>{name}</h2>
+              <p style={{ marginBottom: "1rem", color: "gray" }}>
+                {description}
+              </p>
+              <AddItem>
+                <TextField id="newItem" label="New Item" variant="filled" />
+                <Icon
+                  style={{ fontSize: 30, margin: "auto" }}
+                  onClick={(e) => {
+                    addNewItem(e, _id)
+                  }}
+                >
+                  add_circle
+                </Icon>
+              </AddItem>
+
+              {items.map((item) => (
+                <div
+                  key={item._id}
+                  style={{ margin: "0.5rem 0", display: "block" }}
+                >
+                  <Chip
+                    label={item.itemName}
+                    onClick={handleClick}
+                    onDelete={handleDelete}
+                    color="primary"
+                    variant="outlined"
+                  />
+                </div>
+              ))}
             </CardList>
-           
-            {data.lists.map(({_id, name, description, items, users_permissions_user}) => (
-              (users_permissions_user._id === ID) && (
-                  <CardList elevation={3} key={_id}>
-                    <h2 style={{marginBottom: "0.2rem"}}>{name}</h2>
-                    <p style={{marginBottom: "1rem", color:"gray"}}>{description}</p>
-                    {items.map(item => 
-                    <div key={item._id}  style={{margin: "0.5rem 0", display: "block"}}>
-                      <Chip 
-                        label={item.itemName} 
-                        onClick={handleClick} 
+          ))}
+
+          {data.lists.map(
+            ({ _id, name, description, items, users_permissions_user }) =>
+              users_permissions_user._id === ID && (
+                <CardList elevation={3} key={_id}>
+                  <h2 style={{ marginBottom: "0.2rem" }}>{name}</h2>
+                  <p style={{ marginBottom: "1rem", color: "gray" }}>
+                    {description}
+                  </p>
+                  {items.map((item) => (
+                    <div
+                      key={item._id}
+                      style={{ margin: "0.5rem 0", display: "block" }}
+                    >
+                      <Chip
+                        label={item.itemName}
+                        onClick={handleClick}
                         onDelete={handleDelete}
                         color="primary"
                         variant="outlined"
                       />
                     </div>
-                    )}
-                    
-                  </CardList>
+                  ))}
+                </CardList>
               )
-            ))}
-          </CardWrapper>
-          
-         
+          )}
+        </CardWrapper>
       </CenterContainer>
     </Wrapper>
   )
 }
 
 export default Lists
-
-
-
-
-
-
-// export default function Chips() {
-//   const classes = useStyles();
-
-//   const handleDelete = () => {
-//     console.info('You clicked the delete icon.');
-//   };
-
-//   const handleClick = () => {
-//     console.info('You clicked the Chip.');
-//   };
-
-//   return (
-//     <div className={classes.root}>
-//       <Chip label="Basic" />
-//       <Chip label="Disabled" disabled />
-//       <Chip avatar={<Avatar>M</Avatar>} label="Clickable" onClick={handleClick} />
-//       <Chip
-//         avatar={<Avatar alt="Natacha" src="/static/images/avatar/1.jpg" />}
-//         label="Deletable"
-//         onDelete={handleDelete}
-//       />
-//       <Chip
-//         icon={<FaceIcon />}
-//         label="Clickable deletable"
-//         onClick={handleClick}
-//         onDelete={handleDelete}
-//       />
-//       <Chip
-//         label="Custom delete icon"
-//         onClick={handleClick}
-//         onDelete={handleDelete}
-//         deleteIcon={<DoneIcon />}
-//       />
-//       <Chip label="Clickable Link" component="a" href="#chip" clickable />
-//       <Chip
-//         avatar={<Avatar>M</Avatar>}
-//         label="Primary clickable"
-//         clickable
-//         color="primary"
-//         onDelete={handleDelete}
-//         deleteIcon={<DoneIcon />}
-//       />
-     
-//       <Chip label="Deletable primary" onDelete={handleDelete} color="primary" />
-//       <Chip
-//         icon={<FaceIcon />}
-//         label="Deletable secondary"
-//         onDelete={handleDelete}
-//         color="secondary"
-//       />
-//     </div>
-//   );
-// }
